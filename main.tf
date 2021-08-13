dcf7da363a99e95c890bdfc7c3e8cd2ca5c50d3b
chandradc <52351396+chandradc@users.noreply.github.com>
2021-08-13 14:25:26 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "chandradc-general"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "mycustomvpc"
+  }
+
+}
